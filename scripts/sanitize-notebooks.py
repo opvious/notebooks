c57ccdd0b69de972963d1b166dc9f4b34c87252c
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-06-29 10:22:19 -0700

    Sanitize notebooks

---
+++ scripts/sanitize-notebooks.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""Make notebooks JupyterLite-friendly"""
+
+import json
+import pathlib
+
+_BOUND = ">=0.14.0"
+
+_PREAMBLE_SOURCE_FORMAT = """
+import sys
+if "pyodide" in sys.modules:
+    import piplite
+    await piplite.install('opvious{}')
+"""
+
+def _preamble_cell(bound):
+    return {
+        "cell_type": "code",
+        "metadata": {},
+        "source": _PREAMBLE_SOURCE_FORMAT.format(bound).strip(),
+        "outputs": [],
+        "execution_count": 0,
+    }
+
+def _ensure_preamble(data, preamble):
+    cells = data["cells"]
+    first = _string_value(cells[0]["source"]) if cells else ""
+    if "pyodide" in first:
+        cells[0] = preamble
+    else:
+        cells.insert(0, preamble)
+
+def _string_value(val):
+    return "".join(val) if isinstance(val, list) else val
+
+_MARKDOWN_KEY = "text/markdown"
+
+def _sanitize_markdown_output(data):
+    for cell in data["cells"]:
+        if cell["cell_type"] != "code":
+            continue
+        for output in cell.get("outputs", []):
+            markdown = output.get("data", {}).get(_MARKDOWN_KEY)
+            if markdown:
+                output["data"][_MARKDOWN_KEY] = [_string_value(markdown)]
+
+_root = pathlib.Path(__file__).parent.parent.resolve()
+
+def main():
+    preamble = _preamble_cell(_BOUND)
+    for p in _root.joinpath("notebooks").glob("*.ipynb"):
+        print(f"Sanitizing {p}...")
+        data = json.loads(p.read_text())
+        _ensure_preamble(data, preamble)
+        _sanitize_markdown_output(data)
+        p.write_text(json.dumps(data, indent=2))
+
+if __name__ == '__main__':
+    main()
