2c20fbcd44588500e7acf36290d5a85a15ee615c
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-07-01 21:07:28 -0700

    Add group expense notebook example

--- scripts/sanitize-notebooks.py
+++ scripts/sanitize-notebooks.py
@@ -6,39 +6,25 @@
 import json
 import pathlib
 
-_BOUND = ">=0.15.0"
+def _clear_pip_output(data):
+    for cell in _code_cells(data):
+        if "%pip" in _string_value(cell["source"]):
+            cell["outputs"] = []
 
-_PREAMBLE_SOURCE_FORMAT = """
-import sys
-if "pyodide" in sys.modules:
-    import piplite
-    await piplite.install('opvious{}')
-"""
+def _code_cells(data):
+    for cell in data["cells"]:
+        if cell["cell_type"] == "code":
+            yield cell
 
-def _preamble_cell(bound):
-    return {
-        "cell_type": "code",
-        "metadata": {},
-        "source": _PREAMBLE_SOURCE_FORMAT.format(bound).strip(),
-        "outputs": [],
-        "execution_count": 0,
-    }
-
-def _ensure_preamble(data, preamble):
-    cells = data["cells"]
-    first = _string_value(cells[0]["source"]) if cells else ""
-    if "pyodide" in first:
-        cells[0] = preamble
-    else:
-        cells.insert(0, preamble)
+def _renumber_execution_counts(data):
+    for i, cell in enumerate(_code_cells(data), 1):
+        cell["execution_count"] = i
 
 def _string_value(val):
     return "".join(val) if isinstance(val, list) else val
 
 def _sanitize_markdown_output(data):
-    for cell in data["cells"]:
-        if cell["cell_type"] != "code":
-            continue
+    for cell in _code_cells(data):
         for output in cell.get("outputs", []):
             for key, val in output.get("data", {}).items():
                 output["data"][key] = _string_value(val)
@@ -49,12 +35,12 @@ def _sanitize_markdown_output(data):
 _root = pathlib.Path(__file__).parent.parent.resolve()
 
 def main():
-    preamble = _preamble_cell(_BOUND)
     for p in _root.joinpath("notebooks").glob("*.ipynb"):
         print(f"Sanitizing {p}...")
         data = json.loads(p.read_text())
-        _ensure_preamble(data, preamble)
+        _clear_pip_output(data)
         _sanitize_markdown_output(data)
+        _renumber_execution_counts(data)
         p.write_text(json.dumps(data, indent=2))
 
 if __name__ == '__main__':
