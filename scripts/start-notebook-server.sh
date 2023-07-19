9653161553a5838b0474a8e047481ce452d00c48
Matthieu Monsch <1216372+mtth@users.noreply.github.com>
2023-07-19 13:30:46 -0700

    Add API compatibility test (#22)

--- scripts/start-notebook-server.sh
+++ scripts/start-notebook-server.sh
@@ -8,14 +8,18 @@ set -o errexit
 set -o pipefail
 shopt -s nullglob
 
+__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 main() {
+  cd "$__dirname/.."
   if [ -d venv ]; then
     . venv/bin/activate
   else
     python3 -m venv venv
     . venv/bin/activate
   fi
-  pip install jupyter 'opvious>=0.16.8' pandas
+  pip install jupyter
+  pip install -r requirements.notebooks.txt
   jupyter notebook notebooks
 }
 
