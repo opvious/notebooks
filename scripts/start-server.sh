67f82c8c640f696d2d0affdeb4daae6aae9b1d3e
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-09-04 22:41:32 -0700

    Allow passing options to server script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Convenience script to start a Jupyter notebook server in a self-contained
-# virtualenv.
+# virtualenv. Additional `jupyter notebook` options may be passed when running
+# the command, for example `./scripts/start-server.sh --ip xx.local` to expose
+# the server on the local network.
 
 set -o nounset
 set -o errexit
@@ -10,7 +12,7 @@ shopt -s nullglob
 
 __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-main() {
+main() { # [...]
   cd "$__dirname/.."
   if [ -d venv ]; then
     . venv/bin/activate
@@ -20,7 +22,7 @@ main() {
   fi
   pip install jupyter
   pip install -r requirements.txt
-  jupyter notebook resources
+  jupyter notebook resources "$@"
 }
 
 main "$@"
